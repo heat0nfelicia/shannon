132351a9177a907b4faaadb2ae6e8694d8ed4395
heat0nfelicia <keylea1814@gmail.com>
2025-05-20 08:19:08 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,5 +2,5 @@ import streamlit as st
 
 st.title("konnichiwa")
 st.write(
-    "This is my first app! 😼🗯 im Keyzeea Shannon Felice Simamora from XF"
+    "This is my first app! 😼🗯 its Keyzeea Shannon Felice Simamora from XF"
 )
